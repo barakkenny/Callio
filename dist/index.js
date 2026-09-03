ed05254fac17ad1ec9c83c8eb67e27e65da26b4b
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2026-09-03 23:48:26 +0100

    Setup socket server

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,20 @@ const server_1 = __importDefault(require("./config/server"));
 const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+io.on("connection", (socket) => {
+    console.log('new user connected');
+    socket.on('disconnected', () => {
+        console.log('user disconnected');
+    });
+});
 const { PORT } = server_1.default;
 server.listen(PORT, () => {
     console.log(`server listening at port: ${PORT}`);
