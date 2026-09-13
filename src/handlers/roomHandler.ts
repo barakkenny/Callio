6f0a286a0b022ec372934405abb8959c6eae4ddb
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2026-09-13 22:24:24 +0100

    Add participant to the call

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,17 +1,33 @@
 import { Socket } from "socket.io";
 import {v4 as UUIDv4 } from "uuid"
+import type IRoomParams from "../interface/IRoomParams.js"
 
 const roomHandler = (socket: Socket) => {
+
+    const rooms: Record<string, string[]> = {}
 
     const createRoom = () => {
         const roomId = UUIDv4();
         socket.join(roomId);
+
+        rooms[roomId] = [];
+
         socket.emit("room-created", { roomId });
         console.log("Room created with id: ", roomId)
     }
 
-    const joinedRoom = ({ roomId }: { roomId: string })=> {
-        console.log("New user joined room", {roomId});
+    const joinedRoom = ({ roomId, peerId }: IRoomParams)=> {
+        if(rooms[roomId]){
+            console.log("New user joined room", roomId, 'with peer id as', peerId);
+
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+
+            socket.emit("get-user", {
+                roomId,
+                participants: rooms[roomId]
+            })
+        }
     }
 
     socket.on("create-room", createRoom);
